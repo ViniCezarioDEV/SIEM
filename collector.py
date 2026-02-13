8c76d152bcb424c5ce143a24e8390d8324d0b20b
ViniCezarioDEV <viniciusamcezario@gmail.com>
2026-02-13 19:21:33 -0300

    ADD: adding the feature to get kernel log, and filtering them

--- collector.py
+++ collector.py
@@ -1,9 +1,10 @@
 import re
 
 AUTH_LOG_PATH = "/var/log/auth.log"
+KERNEL_PATH = "/var/log/syslog"
 
 def get_all_auth_logs():
-    with open(AUTH_LOG_PATH, 'r') as file:
+    with open(AUTH_LOG_PATH, 'r', encoding='utf-8', errors='replace') as file:
         lines = file.readlines()
         for i, line in enumerate(lines):
             line = line.replace('\n', '')
@@ -28,13 +29,11 @@ def auth_log_filter(log_lines):
         elif 'unix_chkpwd' in line:                     # when someone enters the wrong password (system)
             filtered_log_lines.append(line)             #
         elif 'systemd-logind' in line:                  # system status (lid opened/closed), the system will (restart/suspend/etc)
-            filtered_log_lines.append(line)             #
-        elif 'cron:session' in line:                    # cron status if anythins was opened/closed by CRON
             filtered_log_lines.append(line)
 
     # second filter - removes irrelevant logs
     for line in filtered_log_lines:
-        if 'buttons' in line or 'seat' in line:
+        if 'buttons' in line or 'seat' in line or 'Lid' in line:
             continue
         if re.search(r'\bc\d+\b', line):
             continue
@@ -42,10 +41,30 @@ def auth_log_filter(log_lines):
 
     return final_filtered
 
+def get_all_sys_logs():
+    with open(KERNEL_PATH, 'r', encoding='utf-8', errors='replace') as file:
+        lines = file.readlines()
+        for i, line in enumerate(lines):
+            line = line.replace('\n', '')
+            lines[i] = line
 
+    return lines
+
+def kernel_log_filter(log_lines):
+    filtered_log_lines = []
+
+    for line in log_lines:
+        if 'New USB device found' in line:
+            filtered_log_lines.append(line)
+
+    return filtered_log_lines
 
 
 auth_log_file_lines = get_all_auth_logs()
 filtered_auth_log_lines = auth_log_filter(auth_log_file_lines)
 
+kernel_file_lines = get_all_sys_logs()
+filtered_kernel_log_lines = kernel_log_filter(kernel_file_lines)
+for i in filtered_kernel_log_lines:
+    print(i)
 
