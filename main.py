c81792edc59d3ba878fbd6cbf92f0825c397a962
ViniCezarioDEV <viniciusamcezario@gmail.com>
2026-02-25 17:57:57 -0300

    MOD: modifying workflow, now its all correct

--- main.py
+++ main.py
@@ -17,57 +17,13 @@ filtered_syslog_log_lines = collector.syslog_log_filter(syslog_file_lines)
 # NORMALAZING LOGS INTO JSON
 for log in filtered_auth_log_lines:
     desassemble_log = normalization.desassemble_log(log)
-
-    # any log with PID information
-    if len(desassemble_log) == 5:
-        timestamp, hostname, service, pid, log_info = desassemble_log
-        normalized_log = {
-            "timestamp": timestamp,
-            "hostname": hostname,
-            "service": service,
-            "pid": pid,
-            "event": log_info
-        }
-
-        print(normalized_log)
-
-    else:
-        timestamp, hostname, service, message = desassemble_log
-        detected_services = normalization.detect_service(message)
-
-        # SUDO incorrect password attempt on terminal log
-        if len(detected_services) == 5:
-            threat_user, event, pwd, target, command = detected_services
-
-            normalized_log = {
-                "timestamp": timestamp,
-                "hostname": hostname,
-                "service": service,
-                "threat_user": threat_user,
-                "target_user": target,
-                "command_executed": command,
-                "pwd": pwd,
-                "event": event,
-            }
-
-        # any else log
-        else:
-            module, submodule, event = detected_services
-
-            normalized_log = {
-                "timestamp": timestamp,
-                "hostname": hostname,
-                "service": service,
-                "module": module,
-                "submodule": submodule,
-                "event": event,
-            }
-
-        print(normalized_log)
+    normalizeted_log = normalization.normalize_log(desassemble_log)
+    normalization.transport_to_json_file(normalizeted_log)
 
 '''
 TODO
 
-esses bang de normalized_log = {} adivinha pra onde tem que ir? KKKKKKKKKKKKKKk
-deveria ta aqui nao
+tem que arrumar o normalization.py para aceitar os logs do syslog
+alguns ele aceita como eu ja fiz o teste,
+mas outros (quais? eu nao sei, tem q ver) ele da erro de UNPACKING
 '''
